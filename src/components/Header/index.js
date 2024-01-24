81a6ab1af6855383ca71877a1112e9193a8ba763
lucas ziro <luscas.guimaraes@gmail.com>
2024-01-24 16:43:17 -0300

    hello there

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 export default function Header() {
   return (
     <Container>
-      <h1>Pipefy Clone</h1>
+      <h1>Zirofy</h1>
     </Container>
   );
 }
